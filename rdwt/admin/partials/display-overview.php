d3c13f57e692f84988449988aa2ab6593a34df03
Robertas <accesspc@gmail.com>
2024-05-22 09:12:11 +0100

    feat: display errors

--- rdwt/admin/partials/display-overview.php
+++ rdwt/admin/partials/display-overview.php
@@ -6,7 +6,8 @@ if ( ! function_exists( 'add_action' ) ) die() ;
 
 <div class="wrap rdwt-admin-wrap">
 
-  <h1><?php esc_html_e( 'Rdev WP Tools', 'rdwt' ); ?></h1>
+  <h1 class="rdwt-title"><?php esc_html_e( 'Rdev WP Tools', 'rdwt' ); ?></h1>
+  <?php settings_errors(); ?>
   <h2 class="nav-tab-wrapper"></h2>
 
   <form method="post" action="options.php">
