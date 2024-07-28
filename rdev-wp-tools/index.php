5bb61a6a2587a52aea45683b8529217bc1cc6fd7
Robertas <accesspc@gmail.com>
2024-07-28 12:45:12 +0100

    clean: phpcs

--- rdev-wp-tools/index.php
+++ rdev-wp-tools/index.php
@@ -1,6 +1,11 @@
 <?php
 /**
  * Silence is golden.
+ * php version 7.3.0
  *
- * @package Rdev\WpTools
+ * @category WpTools
+ * @package  Rdev\WpTools
+ * @author   Robertas Reiciunas <accesspc@gmail.com>
+ * @license  GPL-3.0 http://www.gnu.org/licenses/gpl-3.0.html
+ * @link     https://github.com/accesspc/rdev-wp-tools
  */
